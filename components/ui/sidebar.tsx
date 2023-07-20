84b999faa53935e2c365accc7652a4dbcf36878d
shaddamalbz <shaddam.albz@gmail.com>
2023-07-20 21:24:03 +0700

    opt: mapping menu

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -1,16 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
+import menu from '@/lib/menu.json'
 
 function Sidebar() {
   return (
     <aside className="w-[300px] h-screen bg-slate-100 p-4">
       <div className="grid grid-flow-row gap-4 px-6">
-        <Link className="" href="/products">
-          Products
-        </Link>
-        <Link className="" href="/carts">
-          Carts
-        </Link>
+        {menu.list.map((item, idx) => (
+          <Link href={item.href} key={idx}>
+            {item.label}
+          </Link>
+        ))}
       </div>
     </aside>
   )
